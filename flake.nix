90565e915a911b38c65381b8f0dffaa7a0c34050
wchresta <34962284+wchresta@users.noreply.github.com>
2023-01-14 22:37:38 +0100

    Switch to pyproject style

--- flake.nix
+++ flake.nix
@@ -14,6 +14,9 @@
             version = "0.0.1";
             src = ./.;
 
+            format = "pyproject";
+
+            nativeBuildInputs = with py; [ setuptools ];
             propagatedBuildInputs = with py; [ discordpy ];
 
             checkInputs = with py; [ pytest pytestcov mypy ];
--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# type: ignore
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
